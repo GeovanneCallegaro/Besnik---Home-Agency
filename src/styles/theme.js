41d18e17ecc7f66a39007ed622a450ce2d7e8a3b
Geovanne <callegarog00@gmail.com>
2022-06-07 10:16:04 -0300

    initialize project

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    primaryFontColor: '#000339',
+    bgColor: '#E6F0FF',
+    buttonBgColor: '#0F1B4C',
+  },
+
+  fonts: {
+    primaryFont: "'Poppins', sans-serif",
+  },
+};
